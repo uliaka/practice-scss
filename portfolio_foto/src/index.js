ee23713b3820c04d0d586ab6471d9f66b61b411f
uliaka <uliana.gorin95@gmail.com>
2019-08-06 17:08:42 +0300

    Change menu class

--- portfolio_foto/src/index.js
+++ portfolio_foto/src/index.js
@@ -1,5 +1,15 @@
 require('./index.scss');
 
 $(document).ready(function($) {
-  $('body').css('color', 'green')
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() > 110) {
+      $('#menu').addClass('nav-white');
+      $('a').css('color', '#262525');
+    } else {
+      $('#menu').removeClass('nav-white');
+      $('a').css('color', '#ffffff');
+    }
+  });
+
+
 });
